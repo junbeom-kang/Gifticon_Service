651c5e26b1c1525315237a42f205d8c6cf1ae62b
junbeom-kang <kgood10369@gmail.com>
2021-04-08 02:20:09 +0900

    스프링 부트를 통한 gradle 설정

---
+++ src/main/java/gifticon/giticonshop/GiticonshopApplication.java
@@ -0,0 +1,13 @@
+package gifticon.giticonshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiticonshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiticonshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gifticon/giticonshop/GiticonshopApplicationTests.java
@@ -0,0 +1,13 @@
+package gifticon.giticonshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiticonshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
