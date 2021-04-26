6efbf7642517f5cbb85335e1e93728385757ac41
junbeom-kang <kgood10369@gmail.com>
2021-04-26 23:57:59 +0900

    ItemController 제작

--- src/main/java/gifticon/giticonshop/controller/ItemController.java
+++ src/main/java/gifticon/giticonshop/controller/ItemController.java
@@ -1,7 +1,29 @@
 package gifticon.giticonshop.controller;
 
+import gifticon.giticonshop.service.ItemServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/items")
+@RequiredArgsConstructor
 public class ItemController {
+    private final ItemServiceImpl itemServiceImpl;
+
+    /**
+     * 아이템 목록
+     */
+    @GetMapping("")
+    public String itemList() {
+        return "/items/itemList";
+    }
+
+    @GetMapping("/new")
+    public String newItem(Model model) {
+        ItemForm itemForm()=new ItemForm();
+    }
 }
--- src/main/java/gifticon/giticonshop/controller/ItemForm.java
+++ src/main/java/gifticon/giticonshop/controller/ItemForm.java
@@ -1,2 +1,13 @@
-package gifticon.giticonshop.controller;public class ItemForm {
+package gifticon.giticonshop.controller;
+
+public class ItemForm {
+    private String name;
+    private int price;
+    private int stock;
+
+    public ItemForm(String name, int price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
 }
