3f9596082c815a90e9a42a94ebf2c1923e9b7b15
junbeom-kang <59498625+junbeom-kang@users.noreply.github.com>
2022-07-05 14:40:54 +0900

    Update ItemRepository.java
    web commit  test

--- src/main/java/gifticon/giticonshop/repository/ItemRepository.java
+++ src/main/java/gifticon/giticonshop/repository/ItemRepository.java
@@ -5,6 +5,7 @@ import gifticon.giticonshop.domain.Item;
 import java.util.List;
 
 public interface ItemRepository {
+    //test
     void save(Item item);
     Item findOne(Long id);
     List<Item> findAll();
